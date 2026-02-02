87a2426b63bdf34f8dcf754735e5c1288944cce7
kami-sama <kamisamakirishima@gmail.com>
2026-02-02 14:11:31 +0530

    getUrlFromHtml function and tests

--- cmd/app/crawl.go
+++ cmd/app/crawl.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"io"
 	"net/url"
 	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func (app *application) normalizeUrl(urlS string) (string, error) {
@@ -19,4 +22,32 @@ func (app *application) normalizeUrl(urlS string) (string, error) {
 		path = path[:len(path)-1]
 	}
 	return host + path, nil
+}
+
+func (app *application) getUrlFromHTML(htmlReader io.Reader, baseUrl string) ([]string, error) {
+	urls := []string{}
+	baseUrlStruct, err := url.ParseRequestURI(baseUrl)
+	if err != nil {
+		return nil, err
+	}
+	htmlNode, err := html.Parse(htmlReader)
+	if err != nil {
+		return nil, err
+	}
+	for node := range htmlNode.Descendants() {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, att := range node.Attr {
+				if att.Key == "href" {
+					u, err := url.ParseRequestURI(att.Val)
+					if err != nil {
+						continue
+					}
+
+					u = baseUrlStruct.ResolveReference(u)
+					urls = append(urls, u.String())
+				}
+			}
+		}
+	}
+	return urls, nil
 }
