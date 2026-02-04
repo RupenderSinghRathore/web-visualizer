d1f8da01babef0df8717ea0c4300fb5ee3159b62
kami-sama <kamisamakirishima@gmail.com>
2026-02-04 14:44:31 +0530

    optimized crawler

--- cmd/app/crawl.go
+++ cmd/app/crawl.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -8,20 +10,76 @@ import (
 	"golang.org/x/net/html"
 )
 
-func (app *application) normalizeUrl(urlS string) (string, error) {
-	u, err := url.Parse(urlS)
+func (app *application) normalizeUrl(urlStr string) (string, error) {
+	urlStruct, err := url.Parse(urlStr)
 	if err != nil {
 		return "", err
 	}
-	host := u.Hostname()
+	host := urlStruct.Hostname()
 	if host != "" {
 		host = strings.ToLower(host)
 	}
-	path := u.Path
+	path := urlStruct.Path
 	if path != "" && path[len(path)-1] == '/' {
 		path = path[:len(path)-1]
 	}
 	return host + path, nil
+}
+
+func (app *application) fetchLinks(urlStr string) ([]string, error) {
+	res, err := http.Get(urlStr)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode > 399 {
+		return nil, fmt.Errorf("%d status code", res.StatusCode)
+	}
+	if !isHTML(res.Header.Get("content-type")) {
+		return nil, fmt.Errorf("%s: %s content-type", urlStr, res.Header.Get("content-type"))
+	}
+
+	return app.extractLinksFromBody(res.Body, urlStr)
+}
+
+// needs testing
+func (app *application) extractLinksFromBody(body io.Reader, urlStr string) ([]string, error) {
+	baseUrl, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+	var links []string
+	tokenizer := html.NewTokenizer(body)
+
+	for {
+		tokenType := tokenizer.Next()
+		if tokenType == html.ErrorToken {
+			return links, nil
+		}
+
+		if tokenType == html.StartTagToken {
+			token := tokenizer.Token()
+			if token.Data == "a" {
+				for _, attr := range token.Attr {
+					if attr.Key == "href" {
+						link := attr.Val
+						linkStruct, err := url.Parse(link)
+						if err != nil {
+							app.logger.Error(err)
+							continue
+						}
+						linkStruct = baseUrl.ResolveReference(linkStruct)
+						if linkStruct.Hostname() != baseUrl.Hostname() {
+							continue
+						}
+						link = linkStruct.String()
+						links = append(links, link)
+					}
+				}
+			}
+		}
+	}
 }
 
 func (app *application) crawlPage(baseUrl string) (map[string]struct{}, error) {
@@ -35,60 +93,45 @@ func (app *application) crawlPage(baseUrl string) (map[string]struct{}, error) {
 		return nil, err
 	}
 	pages[normalizedUrl] = struct{}{}
-	queue := []string{urlB.String()}
 
-	for len(queue) != 0 {
-		currUrl := queue[0]
-		queue = queue[1:]
+	result := make(chan []string)
 
-		res, err := http.Get(currUrl)
-		if err != nil {
-			app.logger.Error(err)
-			continue
-		}
-		defer res.Body.Close()
+	workQueue := make(chan string, 100)
+	workQueue <- urlB.String()
 
-		if res.StatusCode > 399 || !isHTML(res.Header.Get("content-type")) {
-			continue
-		}
-
-		htmlNode, err := html.Parse(res.Body)
-		if err != nil {
-			app.logger.Error(err)
-			continue
-		}
-
-		for node := range htmlNode.Descendants() {
-			if node.Type == html.ElementNode && node.Data == "a" {
-				for _, att := range node.Attr {
-					if att.Key == "href" {
-						nextUrl, err := url.ParseRequestURI(att.Val)
-						if err != nil {
-							app.logger.Error(err)
-							continue
-						}
-
-						nextUrl = urlB.ResolveReference(nextUrl)
-
-						if nextUrl.Hostname() != urlB.Hostname() {
-							continue
-						}
-
-						normalizedNextUrl, err := app.normalizeUrl(nextUrl.String())
-						if err != nil {
-							app.logger.Error(err)
-							continue
-						}
-						if _, ok := pages[normalizedNextUrl]; ok {
-							continue
-						}
-						pages[normalizedNextUrl] = struct{}{}
-
-						queue = append(queue, nextUrl.String())
-					}
+	fetching := 1
+	for i := 0; i < app.currencyLimit; i++ {
+		go func() {
+			for urlStr := range workQueue {
+				links, err := app.fetchLinks(urlStr)
+				if err != nil {
+					app.logger.Error(err)
 				}
+				result <- links
 			}
+		}()
+	}
+
+	for fetching > 0 {
+		newLinks := <-result
+		fetching--
+
+		for _, link := range newLinks {
+
+			normalizedLink, err := app.normalizeUrl(link)
+			if err != nil {
+				continue
+			}
+
+			if _, ok := pages[normalizedLink]; !ok {
+				pages[normalizedLink] = struct{}{}
+
+				fetching++
+				go func(l string) { workQueue <- l }(link)
+			}
+
 		}
 	}
+
 	return pages, nil
 }
